4008a3bb6e8cd2e40f3b5cdaa2012535f67dd037
generalov <e.generalov@gmail.com>
2017-01-12 16:41:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 'use strict';
+var path = require('path');
 
 // through2 is a thin wrapper around node transform streams
 var gutil = require('gulp-util');
 var through = require('through2');
 var tfs = require('tfs-unlock');
+var assign = require('object-assign');
+var defaultTo = require('lodash.defaultto');
+var valueOrFunction = require('value-or-function');
 var PluginError = gutil.PluginError;
+var string = valueOrFunction.string;
 
-module.exports = function (opts) {
-	opts = opts || {};
+var PLUGIN_NAME = 'gulp-tfs-checkout';
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+module.exports = function (outFolder, opts) {
+    opts = opts || {};
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-tfs-checkout', 'Streaming not supported'));
-			return;
-		}
+    tfs.init(opts);
 
-		try {
-			tfs.init();
-			tfs.checkout([file.path]).then(function () {
-        gutil.log('Checked out file "' + file.path + '"');
-        cb();
-      }, function (error) {
-        gutil.log(gutil.colors.yellow('Warning: Unable to checkout: ' + file.path + ' - Check that this file is under source control and tf.exe works properlly with this file.'));
-        cb();
-      });
-			
-		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-tfs-checkout', err, {fileName: file.path}));
-		}
-	});
+    return through.obj(function (file, enc, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        } else if (file.isBuffer()) {
+            var options = assign({}, opts, {
+                cwd: defaultTo(string(opts.cwd, file), process.cwd())
+            });
+            var cwd = path.resolve(options.cwd);
+            var outFolderPath = string(outFolder, file);
+            if (!outFolderPath) {
+                throw new Error('Invalid output folder');
+            }
+            var basePath = path.resolve(cwd, outFolderPath);
+            if (!basePath) {
+                throw new Error('Invalid base option');
+            }
+
+            var writePath = path.resolve(basePath, file.relative);
+
+            try {
+                tfs.checkout([writePath]).then(function () {
+                    gutil.log('Checked out file "' + writePath + '"');
+                    callback(null, file);
+                }, function (error) {
+                    gutil.log(gutil.colors.yellow('Warning: Unable to checkout: ' + writePath + ' - Check that this file is under source control and tf.exe works properlly with this file.'));
+                    callback(null, file);
+                });
+            } catch (err) {
+                this.emit('error', new gutil.PluginError(PLUGIN_NAME, err, {
+                    fileName: writePath
+                }));
+            }
+        }
+    });
 };
